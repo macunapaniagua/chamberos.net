8aee663c406c2fa11cc4d225de5b135229643ef4
macunapaniagua <macunapaniagua@gmail.com>
2015-08-04 00:05:59 -0600

    Dashboard navigation bar

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@ var adminUsername = "admin";
 
 window.onload = function (){
 	var loggedUser = localStorage.getItem("logged_user");
-	if(loggedUser === adminUsername){
+	if(loggedUser !== adminUsername){
 		$("#users-module").hide();
 	}
 }
